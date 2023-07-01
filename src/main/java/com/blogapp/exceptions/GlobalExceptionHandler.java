47f814f9acaca91da914688d38bd8ca83e22239a
Dhananjay Sharma <dhananjayrock302@gmail.com>
2023-07-01 22:23:39 +0530

    Modified return response of GlobalExceptionHandler and fix Dto's Validation by adding dependency

--- src/main/java/com/blogapp/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/blogapp/exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.blogapp.exceptions;
 
+import com.blogapp.response.Response;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
+import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,45 +21,95 @@ import java.util.Map;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException (MethodArgumentNotValidException exception){
+    public ResponseEntity<Response> handleMethodArgumentNotValidException (MethodArgumentNotValidException exception){
         Map<String, String> response = new HashMap<>();
         exception.getBindingResult().getAllErrors().forEach((objectError -> {
             String fieldName = ((FieldError) objectError).getField();
             String message = objectError.getDefaultMessage();
             response.put(fieldName, message);
         }));
-        return new ResponseEntity<Map<String, String>>(response, HttpStatus.BAD_REQUEST);
+        return ResponseEntity.ok(
+                Response.builder()
+                        .responseTime(LocalDateTime.now())
+                        .status(HttpStatus.BAD_REQUEST)
+                        .statusCode(HttpStatus.BAD_REQUEST.value())
+                        .message("Input Fields are Not Valid!")
+                        .method("GlobalExceptionHandler.handleMethodArgumentNotValidException")
+                        .executionMessage("Implemented business logic of GlobalExceptionHandler class method")
+                        .data(Collections.singletonMap("error", response))
+                        .build()
+        );
     }
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    public ResponseEntity<Map<String, String>> handleMethodArgumentTypeMismatchException (MethodArgumentTypeMismatchException exception){
+    public ResponseEntity<Response> handleMethodArgumentTypeMismatchException (MethodArgumentTypeMismatchException exception){
         Map<String, String> response = new HashMap<>();
             String fieldName = exception.getName();
             String message = exception.getMessage();
             response.put(fieldName, message);
-        return new ResponseEntity<Map<String, String>>(response, HttpStatus.BAD_REQUEST);
+        return ResponseEntity.ok(
+                Response.builder()
+                        .responseTime(LocalDateTime.now())
+                        .status(HttpStatus.BAD_REQUEST)
+                        .statusCode(HttpStatus.BAD_REQUEST.value())
+                        .message("Arguments types are Mismatch!")
+                        .method("GlobalExceptionHandler.handleMethodArgumentTypeMismatchException")
+                        .executionMessage("Implemented business logic of GlobalExceptionHandler class method")
+                        .data(Collections.singletonMap("error", response))
+                        .build()
+        );
     }
     @ExceptionHandler(MissingServletRequestParameterException.class)
-    public ResponseEntity<Map<String, String>> handleMissingServletRequestParameterException (MissingServletRequestParameterException exception){
+    public ResponseEntity<Response> handleMissingServletRequestParameterException (MissingServletRequestParameterException exception){
         Map<String, String> response = new HashMap<>();
         String parameterName = exception.getParameterName();
         String message = exception.getMessage();
         response.put(parameterName, message);
-        return new ResponseEntity<Map<String, String>>(response, HttpStatus.BAD_REQUEST);
+        return ResponseEntity.ok(
+                Response.builder()
+                        .responseTime(LocalDateTime.now())
+                        .status(HttpStatus.BAD_REQUEST)
+                        .statusCode(HttpStatus.BAD_REQUEST.value())
+                        .message("Missing ServletRequest Parameter!")
+                        .method("GlobalExceptionHandler.handleMissingServletRequestParameterException")
+                        .executionMessage("Implemented business logic of GlobalExceptionHandler class method")
+                        .data(Collections.singletonMap("error", response))
+                        .build()
+        );
     }
     @ExceptionHandler(DataIntegrityViolationException.class)
-    public ResponseEntity<Map<String, String>> handleDataIntegrityViolationException (DataIntegrityViolationException exception){
+    public ResponseEntity<Response> handleDataIntegrityViolationException (DataIntegrityViolationException exception){
         Map<String, String> response = new HashMap<>();
         String name = "DataIntegrityViolationException";
         String message = exception.getMessage();
         response.put(name, message);
-        return new ResponseEntity<Map<String, String>>(response, HttpStatus.BAD_REQUEST);
+        return ResponseEntity.ok(
+                Response.builder()
+                        .responseTime(LocalDateTime.now())
+                        .status(HttpStatus.BAD_REQUEST)
+                        .statusCode(HttpStatus.BAD_REQUEST.value())
+                        .message("DataIntegrity Violation!")
+                        .method("GlobalExceptionHandler.handleDataIntegrityViolationException")
+                        .executionMessage("Implemented business logic of GlobalExceptionHandler class method")
+                        .data(Collections.singletonMap("error", response))
+                        .build()
+        );
     }
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<Map<String, String>> handleHttpMessageNotReadableException (HttpMessageNotReadableException exception){
+    public ResponseEntity<Response> handleHttpMessageNotReadableException (HttpMessageNotReadableException exception){
         Map<String, String> response = new HashMap<>();
         String name = "HttpMessageNotReadableException";
         String message = exception.getMessage();
         response.put(name, message);
-        return new ResponseEntity<Map<String, String>>(response, HttpStatus.BAD_REQUEST);
+        return ResponseEntity.ok(
+                Response.builder()
+                        .responseTime(LocalDateTime.now())
+                        .status(HttpStatus.BAD_REQUEST)
+                        .statusCode(HttpStatus.BAD_REQUEST.value())
+                        .message("HttpMessage Not Readable!")
+                        .method("GlobalExceptionHandler.handleDataIntegrityViolationException")
+                        .executionMessage("Implemented business logic of GlobalExceptionHandler class method")
+                        .data(Collections.singletonMap("error", response))
+                        .build()
+        );
     }
 }
