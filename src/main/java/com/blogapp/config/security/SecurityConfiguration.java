bc6852a00acf3a483ac9d1239d995b10c90d703e
Dhananjay Sharma <dhananjayrock302@gmail.com>
2023-07-27 15:37:59 +0530

    implemented customCorsConfiguration Filter

--- src/main/java/com/blogapp/config/security/SecurityConfiguration.java
+++ src/main/java/com/blogapp/config/security/SecurityConfiguration.java
@@ -3,10 +3,11 @@ package com.blogapp.config.security;
 import com.blogapp.securities.CustomUsrDetailSvc;
 import com.blogapp.securities.JwtAuthenticationEntryPoint;
 import com.blogapp.securities.JwtAuthenticationFilter;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -17,10 +18,15 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @EnableWebSecurity
 @Configuration
-@Slf4j
 public class SecurityConfiguration {
     @Autowired
     private CustomUsrDetailSvc customUsrDetailSvc;
@@ -29,22 +35,30 @@ public class SecurityConfiguration {
     @Autowired
     private JwtAuthenticationFilter jwtAuthenticationFilter;
     public static final String[] PUBLIC_URL = {
-            "/**",
-//            "swagger-ui/**",
-//            "/api",
-//            "swagger-resource/**",
-//            "webjars/**",
-//            "swagger-ui.html"
+            "auth/**",
+            "comments/getByPostId/**",
+            "posts/get_pageable",
+            "categories/get",
+            "posts/image/**",
+            "swagger-ui/**",
+            "/api",
+            "swagger-resource/**",
+            "webjars/**",
+            "swagger-ui.html"
     };
     public static final String[] ADMIN_URL = {
             "categories/add",
-            "category/update",
-            "category/delete",
-            "categories/get",
-            "users/**",
+            "categories/update/**",
+            "categories/delete/**",
+            "users/delete/**",
     };
     public static final String[] SHARED_URL = {
-            "comments/**",
+            "comments/add",
+            "comments/update/**",
+            "comments/delete/**",
+            "posts/add/**",
+            "posts/update/**",
+            "posts/delete/**",
             "users/update/**",
             "categories/get",
     };
@@ -53,10 +67,9 @@ public class SecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .cors(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((authorization) -> authorization
-//                        .requestMatchers(ADMIN_URL).hasRole("ADMIN")
-//                        .requestMatchers(SHARED_URL).hasAnyRole("ADMIN", "USER")
+                        .requestMatchers(ADMIN_URL).hasRole("ADMIN")
+                        .requestMatchers(SHARED_URL).hasAnyRole("ADMIN", "USER")
                         .requestMatchers(PUBLIC_URL).permitAll()
                         .anyRequest().authenticated()
                 )
@@ -66,12 +79,33 @@ public class SecurityConfiguration {
         http.addFilterBefore(this.jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
+
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration builder) throws Exception {
         return builder.getAuthenticationManager();
+    }
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> customCoresFilter() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setMaxAge(3600L);
+
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setBeanName("COCO");
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
     }
 }
